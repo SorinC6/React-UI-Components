2fa075bcf52234b0deb173dab9b998b8116aaa9c
Sorin Chis <kish.sorin@yahoo.com>
2019-02-11 15:52:45 +0200

    importing moment.js library and time-stamp added

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Header.css';
+import moment from 'moment'
+
+const time=moment();
 
 const HeaderTitle = () => (
 	<div className="header-wrapper">
 		<h1 className="header-text">
-			Lambda School <span className="header-time">@LambdaSchool - 26 jan</span>
+			Lambda School <span className="header-time">@LambdaSchool - {time.format('Do MMM')}</span>
 		</h1>
 	</div>
 );

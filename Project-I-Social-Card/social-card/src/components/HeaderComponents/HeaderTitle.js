2c8e41820adca68f443797c14ece77d22c2c8c35
Sorin Chis <kish.sorin@yahoo.com>
2019-02-11 14:31:16 +0200

    added header components content

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => (
+	<div className='header-wrapper'>
+		<h1 className='header-text'>
+			Lambda School <span className='header-time'>@LambdaSchool - 26 jan</span>
+		</h1>
+	</div>
+);
+
+export default HeaderTitle;

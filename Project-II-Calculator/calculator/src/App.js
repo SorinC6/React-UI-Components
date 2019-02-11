6b49a0cf4ebc2e86422fe26db90bcf74a4504f50
Sorin Chis <kish.sorin@yahoo.com>
2019-02-11 17:12:04 +0200

    added display component and Number Buttons component to Calculator

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import './App.css';
 import Display from './components/DisplayComponents/CalculatorDisplay';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
   return (
-    <div>
+    <div className='calculator'>
        <Display/>
+       <div className='button-group'>
+            <div className='left-btn-group'> 
+                 <NumberButton buttonStyle='number-style' number='7'/>
+                 <NumberButton buttonStyle='number-style' number='8'/>
+                 <NumberButton buttonStyle='number-style' number='9'/>
+                 <NumberButton buttonStyle='number-style' number='4'/>
+                 <NumberButton buttonStyle='number-style' number='5'/>
+                 <NumberButton buttonStyle='number-style' number='6'/>
+                 <NumberButton buttonStyle='number-style' number='1'/>
+                 <NumberButton buttonStyle='number-style' number='2'/>
+                 <NumberButton buttonStyle='number-style' number='3'/>
+            </div>
+
+            <div className='right-btn-group'>
+            </div>
+       </div>
     </div>
   );
 };
 
 export default App;
+
+
+     

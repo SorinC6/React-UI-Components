ef3c0eadc97d5058b1746579f00c15241cdd65b6
Sorin Chis <kish.sorin@yahoo.com>
2019-02-11 15:02:10 +0200

    fixing style on Header Component and added Banner to Card Component

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+import CardContainer from './components/CardComponents/CardBanner';
 
 const App = () => {
-  return (
-    <div className='main-container'>
-         <HeaderContainer/>
-    </div>
-  );
+	return (
+		<div className="main-container">
+			<HeaderContainer />
+         <CardContainer/>
+		</div>
+	);
 };
 
 export default App;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -2,13 +2,10 @@ import React from 'react';
 import './Header.css';
 
 const HeaderTitle = () => (
-	<div className='header-wrapper'>
-		<h1 className='header-text'>
-			Lambda School <span className='header-time'>@LambdaSchool - 26 jan</span>
+	<div className="header-wrapper">
+		<h1 className="header-text">
+			Lambda School <span className="header-time">@LambdaSchool - 26 jan</span>
 		</h1>
-      <span>
-         
-      </span>
 	</div>
 );
 
